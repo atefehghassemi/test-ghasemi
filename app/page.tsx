17b9aa101c25b33c6fc4b0255fb429aa7c1f1bd0
عاطفه قاسمی <a.ghasemi@ArianSystemdp.local>
2024-07-22 14:14:13 +0330

    test commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>hello world! di </p>
       یه چیزی بنویس رفتی اونور مطمئن باشی کار خودته
+      <div>الان هر تغییری که بدی و پوش کنی اون طرف میسازه </div>
     </main>
   );
 }
